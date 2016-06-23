79e22bf28911b918bb294be88503e9bea109d544
Brett Neese <brett@neese.rocks>
2016-06-22 19:57:50 -0500

    fix file path

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var fs = require('fs'),
   es = require('event-stream');
 
 var getStream = function () {
-    var jsonData = 'data.json',
+    var jsonData = '../data.json',
         stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
         parser = JSONStream.parse('*');
         return stream.pipe(parser);
